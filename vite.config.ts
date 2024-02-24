03e8d37119c7cfd1a82948fa474b0fc06d1aacae
Lenni009 <guentherhanslphdyt@gmail.com>
2024-02-24 20:01:12 +0100

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/app-template/',
+  base: '/Building-Charts/',
   plugins: [vue()],
   resolve: {
     alias: {
